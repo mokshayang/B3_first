39d4cb1edd054c2d67f272f0c1abde54aca1ff92
moksha <dullstart.yang@gmail.com>
2023-02-15 15:49:20 +0800

    base.php

--- api/get_session.php
+++ api/get_session.php
@@ -1,5 +1,6 @@
 <?php include_once "base.php";
 $row = $Movie->find($_GET['id']);
+$row = $Movie->find($_GET['id']);
 $date = $_GET['date'];
 $hr = date("G");
 if($date === date("Y-m-d") && $hr>=14){
@@ -8,7 +9,8 @@ if($date === date("Y-m-d") && $hr>=14){
     $start = 1;
 }
 for($i=$start;$i<=5;$i++){
-    $tt = $Ord->sum('qt',['id'=>$row['id'],'date'=>$date,'sission'=>$Ord->sss[$i]]);
+    $tt = $Ord->sum('qt',['id'=>$row['id'],'date'=>$date,'session'=>$Ord->sss[$i]]);
+    echo $tt;
     echo "<option value='{$Movie->sss[$i]}'>";
     echo $Movie->sss[$i];
     echo "座位剩 ".(20-$tt);
